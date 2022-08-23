3424ce1950f995c3ee01ca983206f4fef6e963e9
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-08-23 11:13:45 +0300

    lab passed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+
+let names= (["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function writeCards(names, event=surprise){
+    let array=[];
+    for(let i=0; i<names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+function countDown(n) {
+    for (let i=n; i>=0; i--) {
+        console.log (i)
+    } 
+}
+
+
+
+ 
+
